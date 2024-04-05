59649add585b233d6c17e7b3cb21b6a16b87ee7e
Leon Scherer <info@leonscherer.com>
2024-04-05 18:56:16 +0200

    dev

--- packages/web/src/routes/newsletter/[slug]/+page.svelte
+++ packages/web/src/routes/newsletter/[slug]/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-	import EventPage from '$lib/components/EventPage.svelte'
+	import type { EventInfoSpec } from '$lib/components/EventInfo.svelte'
+	import Page from '$lib/components/Page.svelte'
 
 	export let data
+	let {
+		event: { body, title, eventAdmission, eventLocation, eventTime },
+	} = data
+
+	$: eventInfo = {
+		admission: eventAdmission,
+		time: eventTime,
+		location: eventLocation,
+	} satisfies EventInfoSpec
 </script>
 
 <svelte:head>
-	<title>{data.event.title} - Spanischer Verein Köln</title>
+	<title>{title} - Spanischer Verein Köln</title>
 </svelte:head>
 
-<EventPage event={data.event} />
+<Page {body} {title} {eventInfo} />
