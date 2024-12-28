591b2bfe0967b6bbbfc395aa38ef6c2ff61d1043
Leon Scherer <info@leonscherer.com>
2024-12-28 13:17:35 +0100

    add tailwindcss

--- packages/web/src/routes/+layout.svelte
+++ packages/web/src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import "../app.css";
 	import '$lib/styles/globals.scss'
-
+	
 	interface Props {
-		children?: import('svelte').Snippet
+	    children?: import('svelte').Snippet
 	}
-
+	
 	let { children }: Props = $props()
 </script>
 
