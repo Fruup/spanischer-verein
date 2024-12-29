6d7093a810546eebc8dad7334f03404d73751c98
Leon Scherer <info@leonscherer.com>
2024-12-29 20:58:44 +0100

    fixes

--- packages/newsletter/src/data.ts
+++ packages/newsletter/src/data.ts
@@ -28,13 +28,14 @@ export async function sendUnsentNewsletters() {
 			await sendNewsletter({ title, listId, html })
 
 			// Mark as sent.
-			await client
-				.patch(_id, {
-					set: {
-						sentAt: new Date().toISOString(),
-					},
+			const result = await client
+				.patch(_id)
+				.set({
+					sentAt: new Date().toISOString(),
 				})
 				.commit()
+
+			console.log('Patch result:', JSON.stringify(result))
 		} catch (error) {
 			console.error(error)
 			console.error(JSON.stringify(error))
